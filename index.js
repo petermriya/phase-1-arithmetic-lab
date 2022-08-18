64a072765de4abbd41fd4d2dbfa8c07bd20f4856
petermriya <petermuriya@gmail.com>
2022-08-18 22:15:25 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let multiply = 62;
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+
+let random = 2;
+let integer = true;
+
+
+let mod = 4;
+let num3 = 10;
+let num4 = 6;
+
+let max =20;
+(17,18,19,20)
